f5de7fc15425d92b885b77129d02ead228bdda4e
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-04-23 11:51:36 +0300

    _savedTitleInput

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -8,16 +8,32 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var enteredInput = '';
+
+  void _savedTitleInput(String inputValue) {
+    enteredInput = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _savedTitleInput,
             maxLength: 10,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(enteredInput);
+                  },
+                  child: const Text('Saved title'))
+            ],
+          )
         ],
       ),
     );
