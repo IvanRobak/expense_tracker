559ff08d9f5b83e172ac4227aff1c5325c1a50a9
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-04-23 09:01:52 +0300

    ExpenseItem with Card

---
+++ lib/widget/expenses_list/expense_item.dart
@@ -0,0 +1,42 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text('\$${expense.emount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      expense.date.toString(),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
