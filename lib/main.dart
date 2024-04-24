fe7ca99e3b7fe27d79346ecc72affaf17c3e86ab
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-04-24 16:56:32 +0300

    Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:expense_tracker/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expanses(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expanses(),
+    ),
+  );
 }
