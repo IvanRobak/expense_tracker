686239474f1c12ff0ff30233d5b60615a72ff7c1
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-04-24 10:26:38 +0300

    selectedDate

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,11 +11,22 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
-    showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    setState(
+      () {
+        _selectedDate = pickedDate;
+      },
+    );
   }
 
   @override
@@ -55,7 +65,11 @@ class _NewExpenseState extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected date'),
+                    Text(
+                      _selectedDate == null
+                          ? 'No selected date'
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
                       onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month),
